33af2e57aba45a06cef1cbccf852728312fa71a1
Ryna Truong <rynatruong@outlook.com>
2020-07-16 02:47:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,18 @@
 class Cat
+attr_accessor :owner, :mood
+attr_reader :name
+
+@@all = []
+
+  
   def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  
   end
-end
+
+  def self.all
+    @@all
+  end
