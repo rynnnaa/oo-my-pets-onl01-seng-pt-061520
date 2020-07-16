e5ebb6db2350b62d9c90ad8e72087b8c9ad1526a
Ryna Truong <rynatruong@outlook.com>
2020-07-16 02:47:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,38 @@
 class Owner
+  attr_reader :name, :species
+  attr_accessor :cats, :dogs
+  
+  @@all = []
+  
+
   def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
   end
-end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all.select {|cats| cats.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dogs| dogs.owner == self}
+  end
